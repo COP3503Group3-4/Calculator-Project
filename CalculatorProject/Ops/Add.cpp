8e49ac48774a6548f6efb28a06c2514d6930d5f5
Kevin <kmarin@ufl.edu>
2014-04-04 13:07:05 -0400

    started on all the add methods for an operation including a fraction. Feel free to look through it and ccorrect me. I'm kind of winging it.

--- CalculatorProject/Ops/Add.cpp
+++ CalculatorProject/Ops/Add.cpp
@@ -6,6 +6,37 @@
  */
 //test
 
+add::add(){
+
+}
+
+Value add(Fraction a, Fraction b) {
+	int n1 = a.getNumerator() * b.getNumerator;
+	int n2 = b.getNumerator() * a.getDenominator;
+	int d = a.getDenominator() * b.getDenominator;
+	return Fraction(n1+n2, d);
+}
+
+Value add(Fraction a, Expression b) {
+	return Expression(a + b);
+}
+
+Value add(Fraction a, Number b)  {
+	int n1 = a.getNumerator() * b;
+	int n2 = a.getDenominator() * b;
+	int d = a.getDenominator() * b;
+	return Fraction(n1+n2, d);
+}
+
+Value add(Fraction a, Log b)  {
+	return Expression(a,b);
+}
+
+Value add(Fraction a, NthRoot b) {
+	return Expression(a,b);
+}
+
+
 
 
 
